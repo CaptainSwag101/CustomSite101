0bd95948aefb20261684d650be621e7ae6243814
James Pelster <captainswag101@gmail.com>
2024-08-30 21:36:47 -0700

    Copy over existing style and article

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'CustomSite101';
+export const SITE_DESCRIPTION = 'Where I post various ramblings and discussions about my personal interests and hobbies.';
