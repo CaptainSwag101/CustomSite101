d28cc5d2efdd1802a816a9b03afeef98da158aa0
James Pelster <captainswag101@gmail.com>
2025-05-08 12:14:42 -0700

    Update site to use AstroPaper v5

--- src/config.ts
+++ src/config.ts
@@ -1,150 +1,22 @@
-import type { Site, SocialObjects } from "./types";
-
-export const SITE: Site = {
-  website: "https://captainswag101.github.io/CustomSite101", // replace this with your deployed domain
+export const SITE = {
+  website: "localhost", // replace this with your deployed domain
   author: "James Pelster",
   profile: "https://github.com/CaptainSwag101",
   desc: "Where I post various discussions and thoughts about my personal interests and hobbies.",
   title: "CustomSite101",
-  ogImage: "@assets/images/what-is-nassp/thumb.png",
+  ogImage: "social-image.png",
   lightAndDarkMode: true,
   postPerIndex: 4,
-  postPerPage: 6,
+  postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: false,
-};
-
-export const LOCALE = {
+  showBackButton: true, // show back button in post detail
+  editPost: {
+    enabled: false,
+    text: "Suggest Changes",
+    url: "",
+  },
+  dynamicOgImage: true,
   lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-US"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  timezone: "America/Los_Angeles", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
-
-export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 216,
-  height: 46,
-};
-
-export const SOCIALS: SocialObjects = [
-  {
-    name: "Github",
-    href: "https://github.com/CaptainSwag101",
-    linkTitle: `CaptainSwag101 on Github`,
-    active: true,
-  },
-  {
-    name: "LinkedIn",
-    href: "https://linkedin.com/in/james-pelster",
-    linkTitle: `James Pelster on LinkedIn`,
-    active: true,
-  },
-  {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.author}`,
-    active: false,
-  },
-  {
-    name: "Facebook",
-    href: "",
-    linkTitle: `${SITE.author} on Facebook`,
-    active: false,
-  },
-  {
-    name: "Instagram",
-    href: "",
-    linkTitle: `${SITE.author} on Instagram`,
-    active: false,
-  },
-  {
-    name: "Twitter",
-    href: "",
-    linkTitle: `${SITE.author} on Twitter`,
-    active: false,
-  },
-  {
-    name: "Twitch",
-    href: "",
-    linkTitle: `${SITE.author} on Twitch`,
-    active: false,
-  },
-  {
-    name: "YouTube",
-    href: "",
-    linkTitle: `${SITE.author} on YouTube`,
-    active: false,
-  },
-  {
-    name: "WhatsApp",
-    href: "",
-    linkTitle: `${SITE.author} on WhatsApp`,
-    active: false,
-  },
-  {
-    name: "Snapchat",
-    href: "",
-    linkTitle: `${SITE.author} on Snapchat`,
-    active: false,
-  },
-  {
-    name: "Pinterest",
-    href: "",
-    linkTitle: `${SITE.author} on Pinterest`,
-    active: false,
-  },
-  {
-    name: "TikTok",
-    href: "",
-    linkTitle: `${SITE.author} on TikTok`,
-    active: false,
-  },
-  {
-    name: "CodePen",
-    href: "",
-    linkTitle: `${SITE.author} on CodePen`,
-    active: false,
-  },
-  {
-    name: "Discord",
-    href: "",
-    linkTitle: `${SITE.author} on Discord`,
-    active: false,
-  },
-  {
-    name: "GitLab",
-    href: "",
-    linkTitle: `${SITE.author} on GitLab`,
-    active: false,
-  },
-  {
-    name: "Reddit",
-    href: "",
-    linkTitle: `${SITE.author} on Reddit`,
-    active: false,
-  },
-  {
-    name: "Skype",
-    href: "",
-    linkTitle: `${SITE.author} on Skype`,
-    active: false,
-  },
-  {
-    name: "Steam",
-    href: "",
-    linkTitle: `${SITE.author} on Steam`,
-    active: false,
-  },
-  {
-    name: "Telegram",
-    href: "",
-    linkTitle: `${SITE.author} on Telegram`,
-    active: false,
-  },
-  {
-    name: "Mastodon",
-    href: "",
-    linkTitle: `${SITE.author} on Mastodon`,
-    active: false,
-  },
-];
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,11 @@
-import { SITE } from "@config";
-import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+import { SITE } from "@/config";
+
+export const BLOG_PATH = "src/data/blog";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/blog" }),
+  loader: glob({ pattern: "**/[^_]*.md", base: `./${BLOG_PATH}` }),
   schema: ({ image }) =>
     z.object({
       author: z.string().default(SITE.author),
@@ -13,19 +15,11 @@ const blog = defineCollection({
       featured: z.boolean().optional(),
       draft: z.boolean().optional(),
       tags: z.array(z.string()).default(["others"]),
-      ogImage: image()
-        .or(z.string())
-        .optional(),
+      ogImage: image().or(z.string()).optional(),
       description: z.string(),
       canonicalURL: z.string().optional(),
-      editPost: z
-        .object({
-          disabled: z.boolean().optional(),
-          url: z.string().optional(),
-          text: z.string().optional(),
-          appendFilePath: z.boolean().optional(),
-        })
-        .optional(),
+      hideEditPost: z.boolean().optional(),
+      timezone: z.string().optional(),
     }),
 });
 
